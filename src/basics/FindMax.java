50db4956eb950ac68458246fcf427583f2db4840
Ankita <ankitaharihar81@gmail.com>
2026-04-15 14:38:43 +0530

    added new files

---
+++ src/basics/FindMax.java
@@ -0,0 +1,13 @@
+package basics;
+public class FindMax {
+    public static void main(String[] args) {
+       int[] arr = {10,45,67,200,4555,9000};
+       int max = arr[0];
+       for (int i = 1; i < arr.length; i++) {
+           if(arr[i]>max){
+               max = arr[i];
+           }
+       }
+       System.out.println(max);
+    }
+}
