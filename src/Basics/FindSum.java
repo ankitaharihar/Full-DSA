8df7c42684f57722187d5c34d1acccfed40e9da0
Ankita <ankitaharihar81@gmail.com>
2026-04-15 13:54:20 +0530

    FindSum added

--- src/Basics/FindSum.java
+++ src/Basics/FindSum.java
@@ -1,5 +1,15 @@
-class FindSum {
-   public static void main() {
-        int
+package basics;
+
+public class FindSum {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
     }
 }
